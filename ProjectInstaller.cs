61b7fc85d8651ad58746a35e3f6ea8043e8afe65
cosmoden69 <cosmoden@hotmail.com>
2022-04-27 14:39:37 +0900

    초기 커밋.

---
+++ ProjectInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+using System.Linq;
+
+
+namespace YLWEDITransKDB
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+            //var service = new ServiceController(serviceInstaller1.ServiceName);
+            //if (service.Status != ServiceControllerStatus.Running)
+            //{
+            //    service.Start();
+            //}
+        }
+    }
+}
